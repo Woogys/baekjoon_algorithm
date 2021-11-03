f42dac6fe8a88ac49a9e0657107e7e602b4298ed
Woogys <apolontes@hanmail.net>
2021-11-03 15:22:05 +0900

    if문 - 알람시계

---
+++ 2884.py
@@ -0,0 +1,15 @@
+# 2884 if문 - 알람시계
+
+H, M = map(int, input().split())
+
+
+if H >=45:
+    print(H)
+    print(M - 45)
+else:
+    if a == 0:
+        print(24 - 1)
+        print((60 + M) - 45)
+    else:
+        print(H-1)
+        print((60 + M) - 45)
